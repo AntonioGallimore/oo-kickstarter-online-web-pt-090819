72981efc3dbf04c31d371aee3df5f3a55278b5e2
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-16 14:17:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,16 @@
 class Backer
   attr_reader :name, :project
   
-  def initialize(name, project)
+  def initialize(name)
     @name = name
-    @project = project
   end 
   
   def backed_projects
     @backed_projects = []
   end 
+  
+  def project
+    @project = new 
+  end
   
 end 
