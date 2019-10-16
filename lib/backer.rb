a66cef2253bbfa6a2f52506c9dfc24a4f7b69767
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-16 14:34:18 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,10 +3,11 @@ class Backer
   
   def initialize(name)
     @name = name
+    @backed_projects = []
   end 
   
-  def backed_projects
-    @backed_projects = []
+  def backed_projects(project)
+    @backed_projects << project 
   end 
   
   def project
